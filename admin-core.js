26d3a116e23e3facdf4a511cef0a4551f4c38016
iki1uc <entertrainer@web.de>
2026-07-01 03:46:57 +0200

    Add admin initialization function

--- admin-core.js
+++ admin-core.js
@@ -1,1 +1,9 @@
+// admin-core.js
+export function initAdmin(root) {
+  root.innerHTML = `
+    <div style="padding:10px;color:#0f0;font-family:monospace;">
+      ADMIN-Modul aktiv
+    </div>
+  `;
+}
 
